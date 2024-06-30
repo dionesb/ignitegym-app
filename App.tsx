45c9ad241e23d0dad65a957f55e1eecbd3b87907
Antonio Diones Breda <antonio_breda@FCAMARA4820s-MacBook-Pro.local>
2024-06-30 19:04:55 -0300

    personalizando a statusbar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
@@ -11,5 +11,14 @@ export default function App() {
     Roboto_700Bold,
   });
 
-  return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>{fontsLoaded ? <Text>Hello World!</Text> : <View />}</View>;
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#202024" }}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text>Hello World!</Text> : <View />}
+    </View>
+  );
 }
