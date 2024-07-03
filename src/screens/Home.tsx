a11057f2f8af5701a3de478b213199b697341cef
Antonio Diones Breda <antonio_breda@FCAMARA4820s-MacBook-Pro.local>
2024-07-02 22:09:16 -0300

    criando os grupos

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+import { FlatList, HStack, VStack } from "native-base";
+
+// Components
+import Group from "@components/Group";
 import HomeHeader from "@components/HomeHeader";
-import { Center, Text, VStack } from "native-base";
 
 const Home = () => {
+    const [groups, setGroups] = useState(["costa", "ombro", "peito", "bíceps"]);
+    const [groupSelected, setgroupSelected] = useState("costa");
+
     return (
         <VStack flex={1}>
             <HomeHeader />
+            <FlatList
+                data={groups}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <Group
+                        name={item}
+                        isActive={groupSelected === item}
+                        onPress={() => setgroupSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 8 }}
+                my={10}
+                maxH={10}
+            />
         </VStack>
-    )
-}
+    );
+};
 
 export default Home;
